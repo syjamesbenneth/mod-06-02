5cb5eb10f32beca4e523db3177434d5e1c7fd8d9
= <=>
2018-10-08 16:09:48 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+document.getElementById('color-1').onclick = () => {
+	if(document.getElementById('box-1').style.backgroundColor == 'red') {
+		document.getElementById('box-1').style.backgroundColor = 'white';
+	} else {
+		document.getElementById('box-1').style.backgroundColor = 'red';
+	}
+	// document.getElementById('box-1').style.backgroundColor = 'red';
+
+} //this is an example of an anonymous function.
+
+// console.log(document.getElementById)
+
+document.getElementById('color-2').onclick = () => {
+	if(document.getElementById('box-2').style.backgroundColor == 'yellow') {
+		document.getElementById('box-2').style.backgroundColor = 'white';
+	} else {
+		document.getElementById('box-2').style.backgroundColor = 'yellow';
+	}
+}
+
+document.getElementById('color-3').onclick = () => {
+	if(document.getElementById('box-3').style.backgroundColor == 'pink') {
+		document.getElementById('box-3').style.backgroundColor = 'white';
+	} else {
+		document.getElementById('box-3').style.backgroundColor = 'pink';
+	}
+}
+
+document.getElementById('color-4').onclick = () => {
+	if(document.getElementById('box-4').style.backgroundColor == 'green') {
+		document.getElementById('box-4').style.backgroundColor = 'white';
+	} else {
+		document.getElementById('box-4').style.backgroundColor = 'green';
+	}
+}
+
+document.getElementById('color-5').onclick = () => {
+	if(document.getElementById('box-5').style.backgroundColor == 'orange') {
+		document.getElementById('box-5').style.backgroundColor = 'white';
+	} else {
+		document.getElementById('box-5').style.backgroundColor = 'orange';
+	}
+}
+
+document.getElementById('color-6').onclick = () => {
+	if(document.getElementById('box-6').style.backgroundColor == 'purple') {
+		document.getElementById('box-6').style.backgroundColor = 'white';
+	} else {
+		document.getElementById('box-6').style.backgroundColor = 'purple';
+	}
+}
+
+document.getElementById('color-7').onclick = () => {
+	document.getElementById('box-1').style.backgroundColor = 'blue';
+	document.getElementById('box-2').style.backgroundColor = 'blue';
+	document.getElementById('box-3').style.backgroundColor = 'blue';
+	document.getElementById('box-4').style.backgroundColor = 'blue';
+	document.getElementById('box-5').style.backgroundColor = 'blue';
+	document.getElementById('box-6').style.backgroundColor = 'blue';
+	document.getElementById('box-7').style.backgroundColor = 'blue';
+
+	document.getElementById('box-1').style.backgroundColor = 'white';
+	document.getElementById('box-2').style.backgroundColor = 'white';
+	document.getElementById('box-3').style.backgroundColor = 'white';
+	document.getElementById('box-4').style.backgroundColor = 'white';
+	document.getElementById('box-5').style.backgroundColor = 'white';
+	document.getElementById('box-6').style.backgroundColor = 'white';
+	document.getElementById('box-7').style.backgroundColor = 'white';
+
+
+	document.getElementById('box-1').style.backgroundColor = 'blue';
+	document.getElementById('box-2').style.backgroundColor = 'blue';
+	document.getElementById('box-3').style.backgroundColor = 'blue';
+	document.getElementById('box-4').style.backgroundColor = 'blue';
+	document.getElementById('box-5').style.backgroundColor = 'blue';
+	document.getElementById('box-6').style.backgroundColor = 'blue';
+	document.getElementById('box-7').style.backgroundColor = 'blue';
+
+
+}
